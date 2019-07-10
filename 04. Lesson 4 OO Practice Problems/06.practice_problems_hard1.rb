5d7132b9b381041b611e45e93ea422c33c0e87c5
Nathan Worden <worden.nathan@gmail.com>
2019-07-09 19:59:00 -0500

    Lesson 5 Slightly Larger OO Programs

--- 04. Lesson 4 OO Practice Problems/06.practice_problems_hard1.rb
+++ 04. Lesson 4 OO Practice Problems/06.practice_problems_hard1.rb
@@ -1,13 +1,279 @@
-class SecretFile
-  attr_reader :data
+# class SecretFile
+#   def initialize(secret_data, logger)
+#     @data = secret_data
+#     @logger = logger
+#   end
 
-  def initialize(secret_data, SecurityLogger)
-    @data = secret_data
-  end
+#   def data
+#   	@logger.create_log_entry
+#   	@data
+#   end
+# end
+
+# class SecurityLogger
+# 	attr_accessor :log
+
+# 	def initialize
+# 		@log = []
+# 	end
+
+#   def create_log_entry
+#   	log << Time.new
+#   end
+# end
+
+# log = SecurityLogger.new
+# new_access = SecretFile.new('oooSecrets', log)
+# new_access.data
+# new_access.data
+# new_access.data
+# p new_access.data
+# p log.log
+
+
+
+# Question 2
+
+
+# module Moveable
+# 	attr_accessor :speed, :heading
+# 	attr_writer :fuel_capacity, :fuel_efficiency
+
+# 	def range
+# 		@fuel_capacity * @fuel_efficiency
+# 	end
+# end
+
+# class WheeledVehicle
+# 	include Moveable
+
+# 	def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+# 		@tires = tire_array
+# 		self.fuel_efficiency = km_traveled_per_liter
+# 		self.fuel_capacity = liters_of_fuel_capacity
+# 	end
+
+# 	def tire_pressure(tire_index)
+# 		@tires[tire_index]
+# 	end
+
+# 	def inflate_tire(tire_index, pressure)
+# 		@tires[tire_index] = pressure
+# 	end
+# end
+
+# class Auto < WheeledVehicle
+# 	def initialize
+# 		# 4 tires are various tire pressures
+# 		super([30, 30, 32, 32], 50, 25.0)
+# 	end
+# end
+
+# class Motorcycle < WheeledVehicle
+# 	def initialize
+# 		# 2 tires are various tire pressures
+# 		super([20, 20], 80, 8.0)
+# 	end
+# end
+
+# class Catamaran
+# 	include Moveable
+# 	attr_reader :propeller_count, :hull_count
+
+# 	def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+# 		self.fuel_efficiency = km_traveled_per_liter
+# 		self.fuel_capacity = liters_of_fuel_capacity
+# 		# ... code omitted ...
+# 	end
+# end
+
+# car = Auto.new
+# motorcycle = Motorcycle.new
+
+# puts
+
+# p car
+# p motorcycle
+
+# puts
+
+# p "Motorcycle Tire Pressure Before: #{motorcycle.tire_pressure(0)}"
+# motorcycle.inflate_tire(0, 100)
+# p "Motorcycle Tire Pressure After: #{motorcycle.tire_pressure(0)}"
+
+# puts
+
+# p motorcycle.range
+
+
+
+# Question 3
+
+
+# module Moveable
+# 	attr_accessor :speed, :heading
+# 	attr_writer :fuel_capacity, :fuel_efficiency
+
+# 	def range
+# 		@fuel_capacity * @fuel_efficiency
+# 	end
+# end
+
+# class WheeledVehicle
+# 	include Moveable
+
+# 	def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+# 		@tires = tire_array
+# 		self.fuel_efficiency = km_traveled_per_liter
+# 		self.fuel_capacity = liters_of_fuel_capacity
+# 	end
+
+# 	def tire_pressure(tire_index)
+# 		@tires[tire_index]
+# 	end
+
+# 	def inflate_tire(tire_index, pressure)
+# 		@tires[tire_index] = pressure
+# 	end
+# end
+
+# class Auto < WheeledVehicle
+# 	def initialize
+# 		# 4 tires are various tire pressures
+# 		super([30, 30, 32, 32], 50, 25.0)
+# 	end
+# end
+
+# class Motorcycle < WheeledVehicle
+# 	def initialize
+# 		# 2 tires are various tire pressures
+# 		super([20, 20], 80, 8.0)
+# 	end
+# end
+
+# class Seacraft
+# 	include Moveable
+# 	attr_reader :propeller_count, :hull_count
+
+# 	def initialize(num_propellers, num_hulls, fuel_efficiency, fuel_capacity)
+# 		@propeller_count = num_propellers
+# 		@hull_count = num_hulls
+# 		self.fuel_efficiency = fuel_efficiency
+# 		self.fuel_capacity = fuel_capacity
+# 	end
+# end
+
+# class Catamaran < Seacraft
+# end
+
+# class Motorboat < Seacraft
+# 	def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+# 		super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+# 	end
+# end
+
+# water_lily = Catamaran.new(4, 4, 60, 100)
+
+# p water_lily
+# p "The water lily's range is #{water_lily.range} miles"
+
+
+# Question 4
+
+module Moveable
+	attr_accessor :speed, :heading
+	attr_writer :fuel_capacity, :fuel_efficiency
+
+	def range
+		@fuel_capacity * @fuel_efficiency
+	end
 end
 
-class SecurityLogger
-  def create_log_entry
+class WheeledVehicle
+	include Moveable
 
-  end
+	def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+		@tires = tire_array
+		self.fuel_efficiency = km_traveled_per_liter
+		self.fuel_capacity = liters_of_fuel_capacity
+	end
+
+	def tire_pressure(tire_index)
+		@tires[tire_index]
+	end
+
+	def inflate_tire(tire_index, pressure)
+		@tires[tire_index] = pressure
+	end
 end
+
+class Auto < WheeledVehicle
+	def initialize
+		# 4 tires are various tire pressures
+		super([30, 30, 32, 32], 50, 25.0)
+	end
+end
+
+class Motorcycle < WheeledVehicle
+	def initialize
+		# 2 tires are various tire pressures
+		super([20, 20], 80, 8.0)
+	end
+end
+
+class Seacraft
+	include Moveable
+	attr_reader :propeller_count, :hull_count
+
+	def initialize(num_propellers, num_hulls, fuel_efficiency, fuel_capacity)
+		@propeller_count = num_propellers
+		@hull_count = num_hulls
+		self.fuel_efficiency = fuel_efficiency
+		self.fuel_capacity = fuel_capacity
+	end
+
+	def range
+		super + 10
+	end
+end
+
+class Catamaran < Seacraft
+end
+
+class Motorboat < Seacraft
+	def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+		super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+	end
+end
+
+water_lily = Catamaran.new(4, 4, 60, 100)
+
+p water_lily
+p "The water lily's range is #{water_lily.range} miles"
+
+green_motorcycle = Motorcycle.new
+p "The green motorcycle's range is #{green_motorcycle.range} miles"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
