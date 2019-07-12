cd8105b81d2ed1643688ccd781774f4a00aa7d77
Nathan Worden <worden.nathan@gmail.com>
2019-07-12 18:44:27 -0500

    TTT OO

--- 05. Lesson 5 Slightly Larger OO Programs/03.walk_through_OO_TTT_spike.rb
+++ 05. Lesson 5 Slightly Larger OO Programs/03.walk_through_OO_TTT_spike.rb
@@ -1,22 +1,50 @@
+require 'pry'
+
 class Board
+	INITIAL_MARKER = ' '
+
 	def initialize
+		@squares = {}
+		(1..9).each {|key| @squares[key] = Square.new(INITIAL_MARKER)}
+	end
+
+	def get_square_at(key)
+		@squares[key]
+	end
+
+	def set_square_at(key)
+		@squares[key].marker = marker
 	end
 end
 
 class Square
-	def initialize
+	attr_accessor :marker
+	def initialize(marker)
+		@marker = marker
 	end
 end
 
-class Player
-	def initialize
-	end
+def to_s
+	@marker
+end
 
-	def mark
+class Player
+	attr_reader :marker
+
+	def initialize(marker)
+		@marker = marker
 	end
 end
 
 class TTTGame
+	attr_reader :board, :human, :computer
+
+	def initialize
+		@board = Board.new
+		@human = Player.new("X")
+		@computer = Player.new("O")
+	end
+
 	def display_welcome_message
 		puts "Welcome to Tic Tac Toe!"
 		puts ""
@@ -29,28 +57,42 @@ class TTTGame
 	def display_board
 		puts ""
 		puts "     |     |"
-		puts "     |     |"
-		puts "     |     |"
-		puts "-----+-----+-----"
-		puts "     |     |"
-		puts "     |     |"
+		puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
 		puts "     |     |"
 		puts "-----+-----+-----"
 		puts "     |     |"
+		puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
 		puts "     |     |"
+		puts "-----+-----+-----"
+		puts "     |     |"
+		puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
 		puts "     |     |"
 		puts ""
+	end
+
+	def human_moves
+		puts "Choose a square between 1-9"
+		square = nil
+		loop do
+			square = gets.chomp.to_i
+			break if (1..9).include?(square)
+			puts "Sorry, that's not a valid choice."
+		end
+
+		binding.pry
+		board.set_square_at(square, human.marker)
 	end
 
 	def play
 		display_welcome_message
 		loop do
 			display_board
+			human_moves
+			display_board
 			break
-			first_player_moves
 			break if someone_won? || board_full?
 
-			second_player_moves
+			computer_moves
 			break if someone_won? || board_full?
 		end
 		# display_result
