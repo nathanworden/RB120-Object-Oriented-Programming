bfc974e9a0e1fa6696eb5c46a317169376063e31
Nathan Worden <worden.nathan@gmail.com>
2019-07-19 06:17:18 -0500

    OO TTT

--- 05. Lesson 5 Slightly Larger OO Programs/07.assignment_OO_TTT_bonus_features.rb
+++ 05. Lesson 5 Slightly Larger OO Programs/07.assignment_OO_TTT_bonus_features.rb
@@ -1,46 +1,109 @@
-# Tic Tac Toe is a two player game with a 3x3 square board. The goal of the game
-# is to  occupy 3 squares in a row before your opponent does. You and your
-# opponent alternate  taking turns.
-
-# Nouns
-# Player
-# Board
-# Occupy
-# 
-
-
+require 'pry'
 class Board
-  def initialize
+  INITIAL_MARKER = " "
 
+  def initialize
+    @squares = {}
+    (1..9).to_a.each {|key| @squares[key] = Square.new(INITIAL_MARKER)}
+  end
+
+  def get_square_at(key)
+    @squares[key]
+  end
+
+  def set_square_at(key, marker)
+    @squares[key].marker = marker
+  end
+
+  def open_squares
+    @squares.select {|key, value| value.marker == INITIAL_MARKER}
   end
 end
 
 class Square
-  def initialize
+  attr_accessor :marker
 
+  def initialize(marker)
+    @marker = marker
+  end
+
+  def to_s
+    @marker
   end
 end
 
 class Player
-  def initialize
-
+  attr_reader :marker
+  def initialize(marker)
+    @marker = marker
   end
 
-  def mark
-
-  end
-
-  def play
-
-  end
 end
 
 class TTTGame
-  def play
+  HUMAN_MARKER = "X"
+  attr_reader :board, :human, :computer
 
+  def initialize
+    @board = Board.new
+    @human = Player.new("X")
+    @computer = Player.new("O")
+  end
+
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+    puts ""
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+  end
+
+  def display_board
+    puts ""
+    puts "     |     |"
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
+    puts "     |     |"
+    puts ""
+  end
+
+  def human_moves
+    puts "Your move! Your open options are: #{@board.open_squares.keys}"
+    answer = nil
+    loop do
+      answer = gets.chomp.to_i
+      break if board.open_squares.keys.include?(answer)
+      puts "I'm sorry, that square has already been taken."
+    end
+    board.set_square_at(answer, human.marker)
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      display_board
+      human_moves
+      display_board
+      break
+      break if someone_won? || board_full?
+
+      computer_moves
+      break if someone_won? || board_full?
+    end
+    # display_result
+    display_goodbye_message
   end
 end
 
 game = TTTGame.new
+
 game.play
 
