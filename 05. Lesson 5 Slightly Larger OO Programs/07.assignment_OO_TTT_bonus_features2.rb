32c66cf35628e9e08ce04b30f030eed0b49b5b13
Nathan Worden <worden.nathan@gmail.com>
2019-08-02 06:38:24 -0700

    TTT OO Bonus features

--- 05. Lesson 5 Slightly Larger OO Programs/07.assignment_OO_TTT_bonus_features2.rb
+++ 05. Lesson 5 Slightly Larger OO Programs/07.assignment_OO_TTT_bonus_features2.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Board
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
@@ -31,6 +33,22 @@ class Board
         return squares.first.marker
       end
     end
+    nil
+  end
+
+  def find(marker_type)
+    square = nil
+    WINNING_LINES.each do |line|
+      if @squares.values_at(*line).map(&:marker).count(marker_type) == 2
+        square_arr = @squares.select {|k, v| line.include?(k) && v.marker == Square::INITIAL_MARKER}.keys
+        square = square_arr.first if square_arr.size > 0
+      end
+    end
+    square
+  end
+
+  def square_five_open?
+    return 5 if @squares[5].marker == Square::INITIAL_MARKER
     nil
   end
 
@@ -96,15 +114,16 @@ class Player
 end
 
 class TTTGame
-  HUMAN_MARKER = "X"
   COMPUTER_MARKER = "O"
-  FIRST_TO_MOVE = HUMAN_MARKER
+  FIRST_TO_MOVE = "CHOOSE"
 
+  attr_accessor :human_marker
   attr_reader :board, :human, :computer
 
   def initialize
     @board = Board.new
-    @human = Player.new(HUMAN_MARKER)
+    @human_marker = pick_your_marker
+    @human = Player.new(human_marker)
     @computer = Player.new(COMPUTER_MARKER)
     @current_marker = FIRST_TO_MOVE
   end
@@ -114,22 +133,7 @@ class TTTGame
     display_welcome_message
 
     loop do
-
-      loop do
-        display_score
-        display_board
-
-        loop do
-          current_player_moves
-          break if board.someone_won? || board.full?
-          clear_screen_and_display_board
-        end
-
-        display_result
-        break if champion?
-        reset_board
-      end
-
+      outer_game_loop
       display_champion
 
       break unless play_again?
@@ -139,6 +143,31 @@ class TTTGame
     end
 
     display_goodbye_message
+  end
+
+  def inner_game_loop
+    loop do
+      current_player_moves
+      break if board.someone_won? || board.full?
+      clear_screen_and_display_board
+    end
+  end
+
+  def outer_game_loop
+    loop do
+      display_board
+
+      inner_game_loop
+
+      display_result
+      break if champion?
+      reset_board
+    end
+  end
+
+  def pick_your_marker
+    puts "Pick any key on your keyboard to be your marker"
+    human_marker = gets.chomp
   end
 
   def champion?
@@ -172,18 +201,19 @@ class TTTGame
   end
 
   def human_turn?
-    @current_marker == HUMAN_MARKER
+    @current_marker == human_marker
   end
 
   def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
+    display_score
     puts ""
     board.draw
     puts ""
   end
 
   def joinor(arr, separator=', ', last='or')
-    if arr.size <= 2
+    if arr.size == 1
       "#{arr[0]} is your only option"
     elsif arr.size == 2
       arr[0].to_s + " " + last + " " + arr[1].to_s
@@ -191,17 +221,6 @@ class TTTGame
       arr[0..-2].join(separator) + separator + last + " " + arr[-1].to_s
     end
   end
-
-  # def joinor(arr, delimiter=', ', word='or')
-  #   case arr.size
-  #   when 0 then ''
-  #   when 1 then arr.first
-  #   when 2 then arr.join(" #{word} ")
-  #   else
-  #     arr[-1] = "#{word} #{arr.last}"
-  #     arr.join(delimiter)
-  #   end
-  # end
 
   def human_moves
     puts "Choose a square. #{joinor(board.unmarked_keys)}: "
@@ -216,17 +235,44 @@ class TTTGame
   end
 
   def computer_moves
-    board[board.unmarked_keys.sample] = computer.marker
+    square = nil
+
+    if board.winning_square
+      square = board.winning_square
+    elsif board.find_at_risk_square
+      square = board.find_at_risk_square
+    else board.square_five_open?
+      square = board.square_five_open?
+    end
+
+    if !square
+      square = board.unmarked_keys.sample
+    end
+
+    board[square] = computer.marker
   end
 
   def current_player_moves
+    choose_player_to_go_first if @current_marker == "CHOOSE"
     if human_turn?
       human_moves
       @current_marker = COMPUTER_MARKER
     else
       computer_moves
-      @current_marker = HUMAN_MARKER
+      @current_marker = human_marker
     end
+  end
+
+  def choose_player_to_go_first
+    answer = nil
+    loop do
+      puts "Which player should go first? Select 'h' for 'human' and 'c' for 'computer'"
+      answer = gets.chomp
+      break if answer == 'h' || answer == 'c'
+      puts "I'm sorry, you must enter either 'h' or 'c'"
+    end
+    if answer == 'h' then @current_marker = human_marker; end
+    if answer == 'c' then @current_marker = COMPUTER_MARKER; end
   end
 
   def display_result
@@ -243,6 +289,7 @@ class TTTGame
       sleep(1)
     else
       puts "It's a tie!"
+      sleep(1)
     end
   end
 
