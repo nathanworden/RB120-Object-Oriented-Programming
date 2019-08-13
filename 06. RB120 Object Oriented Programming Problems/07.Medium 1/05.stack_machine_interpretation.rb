e4dd90c36f8d07bd2b338a4ec572d28410bd750e
Nathan Worden <worden.nathan@gmail.com>
2019-08-13 05:54:41 -0700

    mini lang OO is hard

--- 06. RB120 Object Oriented Programming Problems/07.Medium 1/05.stack_machine_interpretation.rb
+++ 06. RB120 Object Oriented Programming Problems/07.Medium 1/05.stack_machine_interpretation.rb
@@ -1,48 +1,98 @@
-class Minilang
-  attr_accessor :input
+require 'set'
 
-  def initialize(string)
-    @stack = []
-    @register = [0]
-    @input = string.split.each {|item| item.to_i if item.match(/\d/)}
-    p @input
+class MinilangError < StandardError; end
+class BadTokenError < MinilangError; end
+class EmptyStackError < MinilangError; end
+
+class Minilang
+  ACTIONS = Set.new %w(PUSH ADD SUB MULT DIV MOD POP PRINT)
+  def initialize(program)
+    @program = program
   end
 
   def eval
-    self.send(input)
+    @stack = []
+    @register = 0
+    @program.split.each {|token| eval_token(token)}
+  rescue MinilangError => error
+    puts error.message
   end
 
+  private
+
+  def eval_token(token)
+    if ACTIONS.include?(token)
+      send(token.downcase)
+    elsif token =~ /\A[-+]?\d+\z/
+      @register = token.to_i
+    else
+      raise BadTokenError, "Invalid token: #{token}"
+    end
+  end
+
+  def pop
+    raise EmptyStackError, "Empty stack!" if @stack.empty?
+    @register = @stack.pop
+  end
+
+  def push
+    @stack << @register
+  end
+
+  def add
+    @register += pop
+  end
+
+  def sub
+    @register -= pop
+  end
+
+  def mult
+    @register *= pop
+  end
+
+  def div
+    @register /= pop
+  end
+
+  def mod
+    @register %= pop
+  end
+
+  def print
+    puts @register
+  end
 end
 
 Minilang.new('PRINT').eval
 # 0
 
-# Minilang.new('5 PUSH 3 MULT PRINT').eval
+Minilang.new('5 PUSH 3 MULT PRINT').eval
 # 15
 
-# Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
-# # 5
-# # 3
-# # 8
+Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
+# 5
+# 3
+# 8
 
-# Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
-# # 10
-# # 5
+Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
+# 10
+# 5
 
-# Minilang.new('5 PUSH POP POP PRINT').eval
-# # Empty stack!
+Minilang.new('5 PUSH POP POP PRINT').eval
+# Empty stack!
 
-# Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
-# # 6
+Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
+# 6
 
-# Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
-# # 12
+Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
+# 12
 
-# Minilang.new('-3 PUSH 5 XSUB PRINT').eval
-# # Invalid token: XSUB
+Minilang.new('-3 PUSH 5 XSUB PRINT').eval
+# Invalid token: XSUB
 
-# Minilang.new('-3 PUSH 5 SUB PRINT').eval
-# # 8
+Minilang.new('-3 PUSH 5 SUB PRINT').eval
+# 8
 
-# Minilang.new('6 PUSH').eval
-# # (nothing printed; no PRINT commands)
+Minilang.new('6 PUSH').eval
+# (nothing printed; no PRINT commands)
