75eab033e38bb3f7adebe2a54c67eda03d6b8c69
Nathan Worden <worden.nathan@gmail.com>
2019-08-20 05:34:39 -0700

    Going thorough OO book again

--- 01. Lesson 1 OO Readings/02.Assingment OO Book/02.excercises_oobook1.rb
+++ 01. Lesson 1 OO Readings/02.Assingment OO Book/02.excercises_oobook1.rb
@@ -1,4 +1,10 @@
 class MyCar
+  protected
+  attr_writer :color
+
+  public
+  attr_reader :color
+  attr_reader :year
 
   def initialize(year, color, model)
     @year = year
@@ -9,22 +15,28 @@ class MyCar
 
   def speed_up(number)
     @speed += number
-    puts "The car is currently going #{@speed} miles per hour"
+    puts "You speed up #{number} miles per hour"
   end
 
-  def brake
-    @speed -= 10
+  def brake(number)
+    @speed -= number
+    puts "You hit the brakes and slow down #{number} miles per hour"
+  end
+
+  def current_speed
     puts "The car is currently going #{@speed} miles per hour"
   end
 
   def turn_off
     @speed = 0
     puts "Turning car off.."
-    sleep(1)
-    puts "..."
-    sleep(1)
     puts "The car is off dang it!"
     puts "The car is currently going #{@speed} miles per hour"
+  end
+
+  def spray_paint(color)
+    puts "Yo, we are spray painting your car #{color}"
+    self.color = color
   end
 
   def to_s
@@ -37,6 +49,17 @@ mycar = MyCar.new(2004, 'gold', 'buick')
 puts mycar
 
 mycar.speed_up(15)
+mycar.current_speed
 mycar.speed_up(15)
-mycar.brake
+mycar.current_speed
+mycar.brake(10)
+mycar.current_speed
 mycar.turn_off
+
+puts
+
+puts mycar.color
+mycar.spray_paint('black')
+puts mycar.color
+puts mycar.year
+
